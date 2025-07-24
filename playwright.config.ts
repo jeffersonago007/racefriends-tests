9b385a75861a9e27366d9fbd6a420ca1e37467e6
jeffersonago007 <agostinho.jefferson@gmail.com>
2025-07-24 17:04:49 -0300

    Mudando para ambiente de teste
    Mudando para ambiente de teste

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    baseURL: 'https://racefriends.tripnride.com.br',
+    baseURL: 'https://staging.racefriends.tripnride.com.br',
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
